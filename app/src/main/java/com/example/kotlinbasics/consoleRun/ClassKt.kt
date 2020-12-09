98c60c3dcce7b31815f605d0d8d36590dc23c8b8
gprachan <70263211+gprachan@users.noreply.github.com>
2020-12-09 17:44:18 +0545

    Elaborated comments about secondary constructor

--- app/src/main/java/com/example/kotlinbasics/consoleRun/ClassKt.kt
+++ app/src/main/java/com/example/kotlinbasics/consoleRun/ClassKt.kt
@@ -6,7 +6,7 @@ fun main() {
 
 /*an open keyword enables the extension of class to its child class
 * The examples given below are of primary constructors*/
-open class Animal(color: String) {
+/*open class Animal(color: String) {
     init {
         println("From animal we get: $color")
     }
@@ -14,6 +14,28 @@ open class Animal(color: String) {
 
 class Dog(color: String, breed: String) : Animal(color) {
     init {
+        println("From dog we get: $color and $breed")
+    }
+}*/
+
+/*The examples given below are of secondary constructors
+* The secondary constructor can't have var keyword
+* The secondary constructor has its own body
+* Whenever we are using the secondary constructor we need to call super class constructor*/
+open class Animal {
+    var color: String = ""
+
+    constructor(color: String) {
+        this.color = color
+        println("From animal we get: $color")
+    }
+}
+
+class Dog : Animal {
+    var breed: String = ""
+
+    constructor(color: String, breed: String) : super(color) {
+        this.breed = breed
         println("From dog we get: $color and $breed")
     }
 }
